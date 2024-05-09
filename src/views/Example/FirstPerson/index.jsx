51ee5ed20d7d548c7ff2cac4abb97d206a30dbe2
huamingyi <huamingyi99@163.com>
2024-05-09 16:28:56 +0800

    feat: update

--- src/views/Example/FirstPerson/index.jsx
+++ src/views/Example/FirstPerson/index.jsx
@@ -7,25 +7,47 @@ import { FirstPersonCameraControl } from "../../../assets/js/firstPersonCameraCo
 
 import { OrbitControls, Stars } from "@react-three/drei";
 import { useControls } from "leva"; //快速设置gui
-let personModel = null;
 /**模型操作 */
-function Model() {
+function Model({ result }) {
   /**加载模型 */
-  const result = useLoader(GLTFLoader, "/scene.gltf", (loader) => {
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath("/draco-gltf/");
-    loader.setDRACOLoader(dracoLoader);
-  });
+  return (
+    <>
+      {/* <OrbitControls enabled={orbit} /> */}
+      <primitive object={result} />
+    </>
+  );
+}
 
+/**人物模型 */
+function PersonModel({ result, personModel }) {
+  /**加载人物模型动画 */
+  const modelRef = useRef();
+  useEffect(() => {
+    const actions = {};
+    const model = modelRef.current;
+    const mixer = new THREE.AnimationMixer(model);
+    personModel.animations.forEach((clip) => {
+      actions[clip.name] = mixer.clipAction(clip);
+    });
+    const animate = () => {
+      mixer.update(0.0167); // Update the mixer with the time delta
+      requestAnimationFrame(animate);
+    };
+    animate();
+    actions["TPose"].play(); // Play an animation "Idle"  "Run"  "TPose" "Walk"
+    return () => {
+      mixer.stopAllAction();
+    };
+  }, [personModel]);
   //实例化FirstPersonCameraControl
   const camera = useThree((state) => state.camera);
   const renderer = useThree((state) => {
     return state.gl;
   });
   const firstperson = new FirstPersonCameraControl(camera, renderer.domElement);
-  let orbit = true;
+  firstperson.colliders = result;
 
-  /**gui 可视化实时改变参数 */
+  /**gui可视化实时改变参数 */
   const controls = useControls({
     firstEnabled: false,
     applyGravity: false,
@@ -42,14 +64,12 @@ function Model() {
       firstperson.applyGravity = controls.applyGravity;
       firstperson.applyCollision = controls.applyCollision;
       firstperson.positionEasing = controls.positionEasing;
-      orbit = false;
     } else {
       firstperson.enabled = false;
       var ray = new THREE.Ray();
       console.log("camera.matrixWorld", camera.matrixWorld);
       ray.origin.setFromMatrixPosition(camera.matrixWorld);
       ray.direction.set(0, 0, 1).unproject(camera).sub(ray.origin).normalize();
-      orbit = true;
     }
   }, [
     controls.firstEnabled,
@@ -63,42 +83,6 @@ function Model() {
     firstperson.update();
   });
 
-  /**设置碰撞体 */
-  firstperson.colliders = result.scene;
-  return (
-    <>
-      {/* <OrbitControls enabled={orbit} /> */}
-      <primitive object={result.scene} />
-    </>
-  );
-}
-
-/**人物模型 */
-function PersonModel() {
-  /**加载模型 */
-  personModel = useLoader(GLTFLoader, "/gltf/Soldier.glb", (loader) => {});
-  const modelRef = useRef();
-  useEffect(() => {
-    const model = modelRef.current;
-    const mixer = new THREE.AnimationMixer(model);
-    const actions = {};
-    personModel.animations.forEach((clip) => {
-      actions[clip.name] = mixer.clipAction(clip);
-    });
-
-    const animate = () => {
-      mixer.update(0.0167); // Update the mixer with the time delta
-      requestAnimationFrame(animate);
-    };
-    animate();
-    actions["TPose"].play(); // Play an animation "Idle"  "Run"  "TPose" "Walk"
-    return () => {
-      mixer.stopAllAction();
-    };
-  }, [personModel]);
-  personModel.scene.scale.set(0.6, 0.6, 0.6);
-  personModel.scene.position.set(8.5, 0.4, 1.8);
-  personModel.scene.rotation.y = Math.PI / 2;
   return (
     <>
       <primitive object={personModel.scene} ref={modelRef} />
@@ -119,7 +103,21 @@ function ChangeCamera() {
 
 export default function App() {
   const { axesHelperLength } = useControls({ axesHelperLength: 10 });
-
+  /**加载场景模型 */
+  const result = useLoader(GLTFLoader, "/scene.gltf", (loader) => {
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath("/draco-gltf/");
+    loader.setDRACOLoader(dracoLoader);
+  });
+  /**加载人物模型并初始化位置 */
+  const personModel = useLoader(
+    GLTFLoader,
+    "/gltf/Soldier.glb",
+    (loader) => {}
+  );
+  personModel.scene.scale.set(0.6, 0.6, 0.6);
+  personModel.scene.position.set(8.5, 0.4, 1.8);
+  personModel.scene.rotation.y = Math.PI / 2;
   return (
     <Canvas>
       <ChangeCamera />
@@ -127,8 +125,8 @@ export default function App() {
       <axesHelper args={[axesHelperLength]} />
       <ambientLight intensity={Math.PI} />
       <Suspense fallback={null}>
-        <Model />
-        <PersonModel />
+        <PersonModel result={result.scene} personModel={personModel} />
+        <Model result={result.scene} />
       </Suspense>
     </Canvas>
   );
